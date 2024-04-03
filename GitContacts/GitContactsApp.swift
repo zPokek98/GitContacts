076c46e7478e29c06942e91016461d9de4571068
zPokek98 <iglithcraft@gmail.com>
2024-04-03 10:29:17 +0200

    Initial Commit

---
+++ GitContacts/GitContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitContactsApp.swift
+//  GitContacts
+//
+//  Created by Salvo on 03/04/24.
+//
+
+import SwiftUI
+
+@main
+struct GitContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
